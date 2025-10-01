7b44c146357bf545a3003262e12c85400e7bbe9f
Rohit Kanteti <cs23s019@cse.iitm.ac.in>
2025-10-01 19:36:32 +0530

    Added changes

--- runtime/compiler/runtime/Recompilation_test/operandStack.hpp
+++ runtime/compiler/runtime/Recompilation_test/operandStack.hpp
@@ -4,8 +4,9 @@
 #include "../../../../../omr/compiler/optimizer/PAG/PointerAssignmentGraph.hpp"
 #include <cstdint>
 #include <cassert>
- 
-enum class OperandType {
+
+enum class OperandType
+{
     INT,
     LONG,
     FLOAT,
@@ -14,60 +15,78 @@ enum class OperandType {
     REFERENCE
 };
 
- 
-struct StackFrame {
+struct StackFrame
+{
     OperandType type;
-    union {
+    union
+    {
         int32_t intValue;
         int64_t longValue;
         float floatValue;
         double doubleValue;
         bool boolValue;
-        PAGNode* refValue;
+        PAGNode *refValue;
     } value;
 
-    bool operator<(const StackFrame &other) const {
-        if (type != other.type) return type < other.type;
-        switch (type) {
-            case OperandType::INT: return value.intValue < other.value.intValue;
-            case OperandType::LONG: return value.longValue < other.value.longValue;
-            case OperandType::FLOAT: return value.floatValue < other.value.floatValue;
-            case OperandType::DOUBLE: return value.doubleValue < other.value.doubleValue;
-            case OperandType::BOOLEAN_VALUE: return value.boolValue < other.value.boolValue;
-            case OperandType::REFERENCE: return value.refValue < other.value.refValue;
+    bool operator<(const StackFrame &other) const
+    {
+        if (type != other.type)
+            return type < other.type;
+        switch (type)
+        {
+        case OperandType::INT:
+            return value.intValue < other.value.intValue;
+        case OperandType::LONG:
+            return value.longValue < other.value.longValue;
+        case OperandType::FLOAT:
+            return value.floatValue < other.value.floatValue;
+        case OperandType::DOUBLE:
+            return value.doubleValue < other.value.doubleValue;
+        case OperandType::BOOLEAN_VALUE:
+            return value.boolValue < other.value.boolValue;
+        case OperandType::REFERENCE:
+            return value.refValue < other.value.refValue;
         }
         return false;
     }
+
+    bool isRefOfClass(const std::string &className) const
+    {
+        if (type != OperandType::REFERENCE || value.refValue == nullptr)
+        {
+            return false;
+        }
+        return value.refValue->comp_type.rfind(className, 0) == 0;
+    }
 };
 
-class operandStack {
+class operandStack
+{
 public:
-
     operandStack() = default;
- 
-    operandStack(const operandStack &other);
-    operandStack& operator=(const operandStack &other);
 
+    operandStack(const operandStack &other);
+    operandStack &operator=(const operandStack &other);
 
     void pushInt(int32_t val);
     void pushLong(int64_t val);
     void pushFloat(float val);
     void pushDouble(double val);
     void pushBoolean(bool val);
-    void pushRef(PAGNode* ref);
+    void pushRef(PAGNode *ref);
     void push(const std::set<StackFrame> &s);
-    void push(const std::set<PAGNode*> &s);
-
+    void push(const std::set<PAGNode *> &s);
 
     std::set<StackFrame> pop();
 
-    std::set<int32_t>   popInt();
-    std::set<int64_t>   popLong();
-    std::set<float>     popFloat();
-    std::set<double>    popDouble();
-    std::set<bool>      popBoolean();
-    std::set<PAGNode*>  popRef();
+    std::set<int32_t> popInt();
+    std::set<int64_t> popLong();
+    std::set<float> popFloat();
+    std::set<double> popDouble();
+    std::set<bool> popBoolean();
+    std::set<PAGNode *> popRef();
     bool merge(const operandStack &other);
+
 private:
     std::vector<std::set<StackFrame>> stack;
 };
